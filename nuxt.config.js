3ed2fae336bc81a5dbcc1dd070efd9e9cb8500e2
maegawa <maegawa@b-monster-hack.com>
2021-12-22 13:18:05 +0900

    Add: nuxt-i18n 追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,6 +37,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
+    "nuxt-i18n",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -61,7 +62,20 @@ export default {
       },
     },
   },
-
+  i18n: {
+    locales: ["ja", "en"],
+    defaultLocale: "ja",
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      fallbackLocale: "ja",
+      // silentTranslationWarn: true,
+      silentFallbackWarn: true,
+      messages: {
+        ja: require("./locales/ja.json"),
+        en: require("./locales/en.json"),
+      },
+    },
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 };
